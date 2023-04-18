5116ae1762aa651a1a7d2e4000ec2f3a8806a9f7
rastko <vucen.rastko@gmail.com>
2023-04-18 15:08:11 +0200

    Write a function that works only for one additional argument

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,11 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, toRemove) {
+    for (let i = 0; i < array.length; i++) {
+        if (array[i] === toRemove) {
+            array.splice(i, 1);
+        }
+    }
 
+    return array
 };
 
 // Do not edit below this line
