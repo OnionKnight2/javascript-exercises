3aeb434273dec99567cca01ce734a5c25b9fe96e
rastko <vucen.rastko@gmail.com>
2023-04-18 15:39:30 +0200

    Add logic for non-number types and check which number is bigger

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,12 @@
-const sumAll = function(numberStart, numberEnd) {
+const sumAll = function(numberOne, numberTwo) {
+    if (numberOne < 0 || numberTwo < 0 || typeof numberOne !== 'number' || typeof numberTwo !== 'number') {
+        return "ERROR";
+    }
+
     let sum = 0;
+    const numberStart = (numberOne < numberTwo) ? numberOne : numberTwo;
+    const numberEnd = (numberStart === numberOne) ? numberTwo : numberOne;
+
     for (let i = numberStart; i <= numberEnd; i++) {
         sum += i;
     }
