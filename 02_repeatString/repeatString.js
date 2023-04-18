82f41490564277e3f5e0594b2721ca362d37cc9a
rastko <vucen.rastko@gmail.com>
2023-04-18 14:39:11 +0200

    Complete a function

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,18 @@
-const repeatString = function() {
+const repeatString = function(string, num) {
+    let i = 0;
+    let repeat = "";
 
+    if (num < 0) {
+        return "ERROR";
+    }
+
+    while (i < num) {
+        repeat += string;
+
+        i++;
+    }
+
+    return repeat;
 };
 
 // Do not edit below this line
