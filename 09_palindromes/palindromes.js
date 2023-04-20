329fd7755bea41a7ff832f18e55dd60eb771aab5
rastko <vucen.rastko@gmail.com>
2023-04-20 14:12:04 +0200

    Write a function that determines if a string is a palindrome

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,12 @@
-const palindromes = function () {
+const palindromes = function (sentence) {
+    // Make the string lowercase and split it to array of characters
+    // Filter letter characters
+    const letters = sentence.toLowerCase().split("").filter((letter) => letter.toUpperCase() !== letter.toLowerCase());
 
+    // Check if a string is a palindrome
+    if (letters.join("") === letters.reverse().join("")) {
+        return true;
+    } else return false;
 };
 
 // Do not edit below this line
