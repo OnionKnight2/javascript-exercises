260df9079f9441675efad61ce3b419a8025a252d
rastko <vucen.rastko@gmail.com>
2023-04-20 14:45:47 +0200

    Write a function that returns the oldest person

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,12 @@
-const findTheOldest = function() {
+const findTheOldest = function(people) {
+    const sortedPeople = people.sort((a, b) => {
+        const aDeath = a.yearOfDeath ? a.yearOfDeath : 2023;
+        const bDeath = b.yearOfDeath ? b.yearOfDeath : 2023;
+        
+        return ((bDeath - b.yearOfBirth) - (aDeath - a.yearOfBirth));
+    });
 
+    return sortedPeople[0];
 };
 
 // Do not edit below this line
