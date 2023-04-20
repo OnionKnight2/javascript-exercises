3afa3eb9c204f9366a3e6abb9270b4b792811034
rastko <vucen.rastko@gmail.com>
2023-04-20 14:27:27 +0200

    Create a function that returns a specific member of the Fibonacci sequence

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,16 @@
-const fibonacci = function() {
+const fibonacci = function(a) {
+    // If a number is negative, return some string
+    const aInt = parseInt(a);
+    if (aInt < 0) {
+        return "OOPS";
+    }
 
+    let fib = [1, 1];
+    for (let i = 2; i < a; i++) {
+        fib.push(fib[i - 2] + fib[i - 1]);
+    }
+
+    return fib[a - 1];
 };
 
 // Do not edit below this line
